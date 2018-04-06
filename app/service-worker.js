baf40767379655bff4050a509a930b2f618bf702
Fabien Zibi <fabien.zibi@gmail.com>
2018-04-06 22:12:47 +0200

    Bring back the right solution

--- app/service-worker.js
+++ app/service-worker.js
@@ -49,6 +49,7 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
   const url = new URL(event.request.url);
   const catImage = 'img/cat.jpg';
+  const offlineFile = 'offline.html';
 
   if(url.pathname.includes('socket.io')
       || url.origin.startsWith('chrome-extension')){
@@ -62,7 +63,9 @@ self.addEventListener('fetch', function (event) {
 
     event.respondWith(
       caches.match(event.request).then(function (response) {
-        return response || fetch(event.request);
+        return response || fetch(event.request).catch(function() {
+          return event.respondWith(caches.match(new Request(offlineFile)));
+        });
       })
     );
   }
